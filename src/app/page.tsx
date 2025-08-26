db828e6ad09acc4c7983b565868992c60cd83561
Goutam Dogayan <goutamdogayan.ccpl@gmail.com>
2025-08-26 15:45:49 +0530

    service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+
 import { ServiceCard } from "@/component/Service";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to Patchline</h1>
-
       <ServiceCard />
     </div>
   );
