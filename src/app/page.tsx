279da45d3eb3ca0a90803dd5f7a2a02c003ee444
EMARGER <atulpatel6357@gmail.com>
2025-08-26 17:36:53 +0530

    header file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Header from "@/component/Header";
 import { ServiceCard } from "@/component/Service";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to Patchline</h1>
-
+      
       <ServiceCard />
     </div>
   );
---
+++ src/utilities/data.ts
@@ -0,0 +1,8 @@
+
+export const navItems = [
+  { title: "Home", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "Services", path: "/contact" },
+  { title: "Portfolio", path: "/contact" },
+  { title: "Blog", path: "/contact" },
+];
